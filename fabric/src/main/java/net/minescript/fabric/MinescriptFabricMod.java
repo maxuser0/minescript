16b8783dd94379833db41ae66ac546e06ee3a5f6
Greg Christiana <maxuser@minescript.net>
2022-09-02 08:23:37 -0400

    Code cleanup in MinescriptFabricMod
    Fix debug logging. Remove unused event registrations.

--- fabric/src/main/java/net/minescript/fabric/MinescriptFabricMod.java
+++ fabric/src/main/java/net/minescript/fabric/MinescriptFabricMod.java
@@ -4,7 +4,6 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientChunkEvents;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenKeyboardEvents;
@@ -21,21 +20,7 @@ public final class MinescriptFabricMod implements ClientModInitializer {
 
   @Override
   public void onInitializeClient() {
-    LOGGER.info("(maxuser-dbg) Started MinescriptFabricMod");
-    ScreenEvents.BEFORE_INIT.register(
-        (client, screen, width, height) -> {
-          // TODO: Write tests listening to addition of child elements
-        });
-
-    ClientLifecycleEvents.CLIENT_STARTED.register(
-        client -> {
-          LOGGER.info("(maxuser-dbg) Client started: {}", client);
-        });
-
-    ClientLifecycleEvents.CLIENT_STOPPING.register(
-        client -> {
-          LOGGER.info("(maxuser-dbg) Client stopping: {}", client);
-        });
+    LOGGER.info("(minescript) Minescript mod starting...");
 
     ClientChunkEvents.CHUNK_LOAD.register((world, chunk) -> Minescript.onChunkLoad(world, chunk));
     ClientChunkEvents.CHUNK_UNLOAD.register(
