05c21918e7b99a35567bbcdfe8344256c3d06375
Greg Christiana <maxuser@minescript.net>
2022-09-10 11:03:16 -0400

    Update project version to 2.0
    The previous version was 1.19.2. This changes the numbering scheme to
    diverge from Minecraft version numbers. This establishes semantic
    versioning for Minescript that makes it clear when there's a major vs
    minor update to the mod. Also, it solves the problem of how to specify
    versions when there are multiple Minescript updates, like bug fixes, for
    a single Minecraft version.

--- fabric/src/main/resources/minescript.py
+++ fabric/src/main/resources/minescript.py
@@ -6,7 +6,7 @@
 # make edits to this file, make sure to save a backup copy when upgrading to a
 # new version of Minescript.
 
-"""minescript v1.19.2 distributed via Minescript jar file
+"""minescript v2.0 distributed via Minescript jar file
 
 Usage: import minescript  # from Python script
 
