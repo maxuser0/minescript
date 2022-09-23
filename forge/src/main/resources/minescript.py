cc3aae74097367486a46b170db9efdf0f4731356
Greg Christiana <maxuser@minescript.net>
2022-09-23 09:21:30 -0400

    Deprecate minescript.py's exec in favor of execute
    Previously, doing `from minescript import *` would shadow Python's
    built-in exec() function with minescript.py's exec(). Deprecate
    minescript.exec() in favor of minescript.execute(), with the former
    slated for removal in minescript v3.0. In the meantime, alias the
    built-in exec() as pyexec() so that it remains accessible.

--- forge/src/main/resources/minescript.py
+++ forge/src/main/resources/minescript.py
@@ -21,7 +21,7 @@ from minescript_runtime import CallScriptFunction, CallAsyncScriptFunction
 from typing import Any, List, Set, Dict, Tuple, Optional, Callable
 
 
-def exec(command: str):
+def execute(command: str):
   """Executes the given command.
 
   If command doesn't already start with a slash or backslash, automatically
@@ -34,6 +34,24 @@ def exec(command: str):
   if command[0] not in ("/", "\\"):
     command = "/" + command
   print(command)
+
+
+# Alias the built-in exec as pyexec because exec gets redefined below.
+# TODO(maxuser): Remove this in minescript v3.0.
+pyexec = exec
+
+
+# TODO(maxuser): Remove this in minescript v3.0.
+def exec(command: str):
+  """Executes the given command.
+
+  @deprecated: Use minescript.execute to avoid conflict with built-in exec.
+
+  If command doesn't already start with a slash or backslash, automatically
+  prepends a slash. Ignores leading and trailing whitespace, and ignores empty
+  commands.
+  """
+  return execute(command)
 
 
 def echo(message: Any):
