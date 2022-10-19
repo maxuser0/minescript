2e38c6c25c18fac6056e0b6dcf941410a9a7924b
Greg Christiana <maxuser@minescript.net>
2022-10-19 00:57:52 -0400

    Refactor Volume into BlockPacker and BlockPack
    BlockPacker manages a mutable set of blocks, and packs them into a
    BlockPack which is an immutable set of blocks.

--- fabric/src/main/java/net/minescript/common/BlockPacker.java
+++ fabric/src/main/java/net/minescript/common/BlockPacker.java
@@ -4,56 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
-// TODO(maxuser): Rename Volume to BlockPacker, and change Volume.printBlockCommands() -> void to
-// BlockPacker.pack() -> BlockPack. BlockPack manages a Map<Long, BlockPack.Tile>.
-// BlockPack.Tile contains these fields:
-//
-// 1. String[] blockTypes: max size 2^15 == 32768 (max blocks in 32*32*32 volume);
-// or int[] blockTypeIndices: indices into a BlockPack-wide String[] to eliminate String
-// duplication.
-//
-// 2. int firstUnstableBlockType: index into blockTypes corresponding to the first index of unstable
-// blocks; all block types at indices below firstUnstableBlockType are treated as stable blocks.
-// See #Layering below for details.
-//
-// 3. int[] setblocks: (or 2*16-bit for consistency with fills array)
-// - bits 0-14: block type value within 32*32*32 BlockPack.Tile
-// - bit 15: reserved
-// - bits 16-20: x position within BlockPack.Tile
-// - bits 21-25: y position within BlockPack.Tile
-// - bits 26-30: z position within BlockPack.Tile
-// - bit 31: reserved
-//
-// 4. short[] fills: 3*16-bit fill-style volumes:
-// - bits 0-14: block type value within 32*32*32 BlockPack.Tile
-// - bit 15: reserved
-// - bits 16-20: min x position within BlockPack.Tile
-// - bits 21-25: min y position within BlockPack.Tile
-// - bits 26-30: min z position within BlockPack.Tile
-// - bit 31: reserved
-// - bits 32-36: max x position within BlockPack.Tile
-// - bits 37-41: max y position within BlockPack.Tile
-// - bits 42-46: max z position within BlockPack.Tile
-// - bit 47: reserved
-//
-// BlockPack is not amenable to repeated random access by x, y, z position. Code (in Java or Python)
-// that wishes to visit blocks repeatedly at specific positions should expand a BlockPack into a
-// format that's optimized for random access by inflating BlockPack.Tile into a position-indexed
-// array by iterating over Tile.setblocks and Tile.fills.
-//
-// #Layering
-// When a BlockMap is placed as blocks into a world, volumes in tiles must be placed in two
-// sequential layers: all volumes corresponding to the first layer must be placed before any of the
-// volumes corresponding to the second layer.  The first layer places "stable blocks" that do not
-// fall, do not flow, and do not require adjacent blocks for placements; the second layer places
-// "unstable blocks" which can fall, flow, or require adjacent blocks for placement. (A third,
-// middle layer may be used for unknown blocks like those defined in mods.) Volumes are stored
-// within a BlockMap's tiles irrespective of layer; layers are imposed by iterating volumes across
-// tiles twice, each time with a mutually exclusive filter applied. Values within Tile.blockTypes[]
-// are ordered so that stable block types have lower indices than unstable block types; all indices
-// less than Tile.firstUnstableBlockType correspond to stable blocks; all indices at least
-// Tile.firstUnstableBlockType correspond to unstable blocks.
-public class Volume {
+/**
+ * {@code BlockPacker} manages blocks and packs them into a {@code BlockPack}.
+ * <p>
+ * While {@code BlockPacker} manages a dynamic set of blocks, {@code BlockPack} is immutable.
+ */
+public class BlockPacker {
   private static final int X_BUILD_MIN = Integer.MIN_VALUE;
   private static final int X_BUILD_MAX = Integer.MAX_VALUE;
   private static final int Y_BUILD_MIN = -64;
@@ -66,19 +22,14 @@ public class Volume {
   private final int yTileSize;
   private final int zTileSize;
 
-  private boolean offsetToOrigin = false;
   private int minX = X_BUILD_MAX;
   private int minY = Y_BUILD_MAX;
   private int minZ = Z_BUILD_MAX;
 
-  public Volume(int xTileSize, int yTileSize, int zTileSize) {
+  public BlockPacker(int xTileSize, int yTileSize, int zTileSize) {
     this.xTileSize = xTileSize;
     this.yTileSize = yTileSize;
     this.zTileSize = zTileSize;
-  }
-
-  public void offsetToOrigin(boolean enable) {
-    this.offsetToOrigin = enable;
   }
 
   public void addBlock(int x, int y, int z, String blockType) {
@@ -133,28 +84,37 @@ public class Volume {
         | ((long) zTile & 0x7ffffff);
   }
 
-  // TODO(maxuser): Allow continuous / iterative recomputation of block commands.
-  public void build() {
+  public void printDebugInfo(boolean offsetToOrigin) {
     for (Tile tile : tiles.values()) {
-      tile.computeRunLengths();
-      tile.computeBlockCommands();
-    }
-  }
-
-  public void printBlockCommands() {
-    build();
-    for (Tile tile : tiles.values()) {
-      tile.printBlockCommands(offsetToOrigin, minX, minY, minZ);
-    }
-  }
-
-  public void printBlockCommandsWithDebugInfo() {
-    build();
-    for (Tile tile : tiles.values()) {
-      System.out.printf("# tile offset: %d %d %d\n", tile.xOffset, tile.yOffset, tile.zOffset);
+      int x = tile.xOffset;
+      int y = tile.yOffset;
+      int z = tile.zOffset;
+      if (offsetToOrigin) {
+        x -= minX;
+        y -= minY;
+        z -= minZ;
+      }
+      System.out.printf("# tile offset: %d %d %d\n", x, y, z);
       System.out.print(tile.getDebugInfo());
-      tile.printBlockCommands(offsetToOrigin, minX, minY, minZ);
     }
+  }
+
+  // TODO(maxuser): Allow re-packing of blocks from the same BlockPacker after adding more blocks.
+  public BlockPack pack() {
+    // TODO(mxuser): Consider transforming tile.types into a symbol table attached to entire
+    // BlockPack rather than each BlockPack.Tile.
+    var packedTiles = new TreeMap<Long, BlockPack.Tile>();
+    for (var entry : tiles.entrySet()) {
+      long key = entry.getKey();
+      var tile = entry.getValue();
+
+      tile.computeRunLengths();
+      tile.updateTypeList();
+      var packedTile = new BlockPack.Tile(tile.types, tile.xOffset, tile.yOffset, tile.zOffset);
+      tile.computeBlockCommands(packedTile);
+      packedTiles.put(key, packedTile);
+    }
+    return new BlockPack(minX, minY, minZ, packedTiles);
   }
 
   public static class Tile {
@@ -184,39 +144,6 @@ public class Volume {
     // - bits 25-29: consecutive blocks of same type in +z direction
     // - bit 30: 1 if this block has been filled, 0 otherwise
     private final int[] blocks;
-
-    // Every 4 ints represents: x, y, z, block_type
-    // TODO(maxuser): Consider subclassing IntList with SetblockList.
-    private IntList setblocks = new IntList();
-
-    // Every 7 ints represents: x1, y1, z1, x2, y2, z2, block_type
-    // TODO(maxuser): Consider subclassing IntList with FillList.
-    private IntList fills = new IntList();
-
-    private static class IntList {
-      private int size = 0;
-      private int[] ints = new int[64];
-
-      public IntList() {}
-
-      public IntList add(int i) {
-        if (size >= ints.length) {
-          int[] newInts = new int[2 * ints.length];
-          System.arraycopy(ints, 0, newInts, 0, ints.length);
-          ints = newInts;
-        }
-        ints[size++] = i;
-        return this;
-      }
-
-      public int size() {
-        return size;
-      }
-
-      public int get(int i) {
-        return ints[i];
-      }
-    }
 
     public Tile(int xOffset, int yOffset, int zOffset) {
       this(xOffset, yOffset, zOffset, 16, 16, 16);
@@ -360,7 +287,7 @@ public class Volume {
       }
     }
 
-    public void computeBlockCommands() {
+    public void computeBlockCommands(BlockPack.Tile tile) {
       // Use the runs of consecutive blocks of the same type computed above to identify x,z-plane
       // areas and x,y,z rectangular volumes of the same type of block.
       for (int y = 0; y < ySize; ++y) {
@@ -422,9 +349,9 @@ public class Volume {
               final int dy = minYRun;
               final int dz = maxAreaZ - z;
               if (dx == 0 && dy == 0 && dz == 0) {
-                setblocks.add(x).add(y).add(z).add(blockType);
+                tile.setblocks.add(x).add(y).add(z).add(blockType);
               } else {
-                fills
+                tile.fills
                     .add(x)
                     .add(y)
                     .add(z)
@@ -443,42 +370,6 @@ public class Volume {
             }
           }
         }
-      }
-    }
-
-    public void printBlockCommands(boolean offsetToOrigin, int minX, int minY, int minZ) {
-      updateTypeList();
-      for (int i = 0; i < fills.size(); i += 7) {
-        int x1 = xOffset + fills.get(i);
-        int y1 = yOffset + fills.get(i + 1);
-        int z1 = zOffset + fills.get(i + 2);
-        int x2 = xOffset + fills.get(i + 3);
-        int y2 = yOffset + fills.get(i + 4);
-        int z2 = zOffset + fills.get(i + 5);
-        if (offsetToOrigin) {
-          x1 -= minX;
-          y1 -= minY;
-          z1 -= minZ;
-          x2 -= minX;
-          y2 -= minY;
-          z2 -= minZ;
-        }
-        String blockType = types[fills.get(i + 6)];
-        System.out.printf(
-            "/fill %d %d %d %d %d %d %s\n",
-            x1, y1, z1, x2, y2, z2, blockType, (x2 - x1 + 1) * (y2 - y1 + 1) * (z2 - z1 + 1));
-      }
-      for (int i = 0; i < setblocks.size(); i += 4) {
-        int x = xOffset + setblocks.get(i);
-        int y = yOffset + setblocks.get(i + 1);
-        int z = zOffset + setblocks.get(i + 2);
-        if (offsetToOrigin) {
-          x -= minX;
-          y -= minY;
-          z -= minZ;
-        }
-        String blockType = types[setblocks.get(i + 3)];
-        System.out.printf("/setblock %d %d %d %s\n", x, y, z, blockType);
       }
     }
 
---
+++ forge/src/main/java/net/minescript/common/BlockPack.java
@@ -0,0 +1,181 @@
+package net.minescript.common;
+
+import java.util.TreeMap;
+
+/**
+ * {@code BlockPack} represents blocks stored as rectangular volumes of equivalent blocks.
+ * <p>
+ * {@code BlockPack} stores blocks for efficient translation into /fill and /setblock commands.
+ */
+public class BlockPack {
+  private final int minX;
+  private final int minY;
+  private final int minZ;
+
+  // TODO(maxuser): Currently using a TreeMap to preserve ordering, but the current ordering is
+  // determined by the code that instantiates a BlockPack. For instance, the keys defined by
+  // BlockPacker.getTileKey() intentionally encode the tile's y offset as the high bits of the key.
+  // The y-first tile order means that blocks can be rendered naturally in bottom-up order so
+  // flowing blocks like water in higher tiles follow stable blocks beneath them in lower tiles. But
+  // other code that instantiates a BlockPack might order keys differently, so we should enforce
+  // y-first key order within BlockPack itself.
+  private final TreeMap<Long, Tile> tiles;
+
+  public BlockPack(int minX, int minY, int minZ, TreeMap<Long, Tile> tiles) {
+    this.minX = minX;
+    this.minY = minY;
+    this.minZ = minZ;
+    this.tiles = tiles;
+  }
+
+  // TODO(maxuser): Replace printBlockCommands with general-purpose iterability of tiles in layers
+  // (see #Layering below).
+  public void printBlockCommands(boolean offsetToOrigin, boolean setblockOnly) {
+    for (Tile tile : tiles.values()) {
+      tile.printBlockCommands(offsetToOrigin, setblockOnly, minX, minY, minZ);
+    }
+  }
+
+  // TODO(maxuser): Update BlockPack.Tile to contain these fields:
+  //
+  // 1. ImmutableList<String> blockTypes: max size 2^15 == 32768 (max blocks in 32*32*32 volume);
+  // or int[] blockTypeIndices: indices into a BlockPack-wide list/array of String to eliminate
+  // String duplication. (It's ok for blockTypes to remain as String[] if it remains an
+  // implementation detail and callers don't need to iterate or lookup values.)
+  //
+  // 2. int firstUnstableBlockType: index into blockTypes corresponding to the first index of
+  // unstable blocks; all block types at indices below firstUnstableBlockType are treated as stable
+  // blocks.  See #Layering below for details.
+  //
+  // 3. int[] setblocks: (or 2*16-bit for consistency with fills array)
+  // - bits 0-14: block type value within 32*32*32 BlockPack.Tile
+  // - bit 15: reserved
+  // - bits 16-20: x position within BlockPack.Tile
+  // - bits 21-25: y position within BlockPack.Tile
+  // - bits 26-30: z position within BlockPack.Tile
+  // - bit 31: reserved
+  //
+  // 4. short[] fills: 3*16-bit fill-style volumes:
+  // - bits 0-14: block type value within 32*32*32 BlockPack.Tile
+  // - bit 15: reserved
+  // - bits 16-20: min x position within BlockPack.Tile
+  // - bits 21-25: min y position within BlockPack.Tile
+  // - bits 26-30: min z position within BlockPack.Tile
+  // - bit 31: reserved
+  // - bits 32-36: max x position within BlockPack.Tile
+  // - bits 37-41: max y position within BlockPack.Tile
+  // - bits 42-46: max z position within BlockPack.Tile
+  // - bit 47: reserved
+  //
+  // BlockPack is not designed for repeated random access by x, y, z position. Code (in Java or
+  // Python) that wishes to visit blocks repeatedly at specific positions should expand a BlockPack
+  // into a format that's optimized for random access by inflating BlockPack.Tile into a
+  // position-indexed array by iterating over Tile.setblocks and Tile.fills.
+  //
+  // #Layering
+  // When a BlockPack is placed as blocks into a world, volumes in tiles must be placed in two
+  // sequential layers: all volumes corresponding to the first layer must be placed before any of
+  // the volumes corresponding to the second layer.  The first layer places "stable blocks" that do
+  // not fall, do not flow, and do not require adjacent blocks for placements; the second layer
+  // places "unstable blocks" which can fall, flow, or require adjacent blocks for placement. (A
+  // third, middle layer may be used for unknown blocks like those defined in mods.) Volumes are
+  // stored within a BlockPack's tiles irrespective of layer; layers are imposed by iterating
+  // volumes across tiles twice, each time with a mutually exclusive filter applied. Values within
+  // Tile.blockTypes[] are ordered so that stable block types have lower indices than unstable block
+  // types; all indices less than Tile.firstUnstableBlockType correspond to stable blocks; all
+  // indices at least Tile.firstUnstableBlockType correspond to unstable blocks.
+  public static class Tile {
+    private final int xOffset;
+    private final int yOffset;
+    private final int zOffset;
+
+    // TODO(maxuser): Consider using ImmutableList<String> for immutability.
+    private final String[] blockTypes;
+
+    // Every 4 ints represents: x, y, z, block_type
+    // TODO(maxuser): Don't need 4 32-bit ints since each value requires only 5 bits.
+    // TODO(maxuser): Consider subclassing IntList with SetblockList.
+    public IntList setblocks = new IntList();
+
+    // Every 7 ints represents: x1, y1, z1, x2, y2, z2, block_type
+    // TODO(maxuser): Don't need 7 32-bit ints since each value requires only 5 bits.
+    // TODO(maxuser): Consider subclassing IntList with FillList.
+    public IntList fills = new IntList();
+
+    public Tile(String[] blockTypes, int xOffset, int yOffset, int zOffset) {
+      this.blockTypes = blockTypes;
+      this.xOffset = xOffset;
+      this.yOffset = yOffset;
+      this.zOffset = zOffset;
+    }
+
+    public static class IntList {
+      private int size = 0;
+      private int[] ints = new int[64];
+
+      public IntList() {}
+
+      public IntList add(int i) {
+        if (size >= ints.length) {
+          int[] newInts = new int[2 * ints.length];
+          System.arraycopy(ints, 0, newInts, 0, ints.length);
+          ints = newInts;
+        }
+        ints[size++] = i;
+        return this;
+      }
+
+      public int size() {
+        return size;
+      }
+
+      public int get(int i) {
+        return ints[i];
+      }
+    }
+
+    public void printBlockCommands(
+        boolean offsetToOrigin, boolean setblockOnly, int minX, int minY, int minZ) {
+      for (int i = 0; i < fills.size(); i += 7) {
+        int x1 = xOffset + fills.get(i);
+        int y1 = yOffset + fills.get(i + 1);
+        int z1 = zOffset + fills.get(i + 2);
+        int x2 = xOffset + fills.get(i + 3);
+        int y2 = yOffset + fills.get(i + 4);
+        int z2 = zOffset + fills.get(i + 5);
+        if (offsetToOrigin) {
+          x1 -= minX;
+          y1 -= minY;
+          z1 -= minZ;
+          x2 -= minX;
+          y2 -= minY;
+          z2 -= minZ;
+        }
+        String blockType = blockTypes[fills.get(i + 6)];
+        if (setblockOnly) {
+          for (int x = x1; x <= x2; ++x) {
+            for (int y = y1; y <= y2; ++y) {
+              for (int z = z1; z <= z2; ++z) {
+                System.out.printf("/setblock %d %d %d %s\n", x, y, z, blockType);
+              }
+            }
+          }
+        } else {
+          System.out.printf("/fill %d %d %d %d %d %d %s\n", x1, y1, z1, x2, y2, z2, blockType);
+        }
+      }
+      for (int i = 0; i < setblocks.size(); i += 4) {
+        int x = xOffset + setblocks.get(i);
+        int y = yOffset + setblocks.get(i + 1);
+        int z = zOffset + setblocks.get(i + 2);
+        if (offsetToOrigin) {
+          x -= minX;
+          y -= minY;
+          z -= minZ;
+        }
+        String blockType = blockTypes[setblocks.get(i + 3)];
+        System.out.printf("/setblock %d %d %d %s\n", x, y, z, blockType);
+      }
+    }
+  }
+}
