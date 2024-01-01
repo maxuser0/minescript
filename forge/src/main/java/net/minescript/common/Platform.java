a2ae9812cf854da05fbc2f3f31e8db7b6fa5d64d
Greg Christiana <maxuser@minescript.net>
2023-12-31 23:51:08 -0500

    Refactor common sources into common project dir
    Use the MultiLoader-Template as the basis for refactoring sources and
    build configuration into a top-level project with subprojects for
    fabric, forge, and common. The original project template can be found
    at:
    
    https://github.com/jaredlll08/MultiLoader-Template
    
    Fabric and Forge mods can be built with the following commands:
    
    ```
    $ ./gradlew fabric:build
    $ ./gradlew forge:build
    ```
    
    Or build all subprojects:
    
    ```
    $ ./gradlew forge:build
    ```
    
    Fabric and Forge mods can be run in dev mode with the following
    commands:
    
    ```
    $ ./gradlew fabric:runClient
    $ ./gradlew forge:runClient
    ```
    
    Previously, common sources were duplicated across fabric and forge
    subdirectories, and there were neither common nor top-level gradle
    projects.

--- forge/src/main/java/net/minescript/common/Platform.java
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: © 2022-2023 Greg Christiana <maxuser@minescript.net>
-// SPDX-License-Identifier: GPL-3.0-only
-
-package net.minescript.common;
-
-public interface Platform {
-  String getChatScreenInputFieldName();
-}
