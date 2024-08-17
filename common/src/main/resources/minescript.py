c35865008a63ce958f1dc41d32a87742182e4dfb
Greg Christiana <maxuser@minescript.net>
2024-08-16 22:14:38 -0400

    Document key_mapping_name values to press_key_bind

--- common/src/main/resources/minescript.py
+++ common/src/main/resources/minescript.py
@@ -329,8 +329,17 @@ player_inventory_select_slot = ScriptFunction(
 def press_key_bind(key_mapping_name: str, pressed: bool):
   """Presses/unpresses a mapped key binding.
 
+  Valid values of `key_mapping_name` include: "key.advancements", "key.attack", "key.back",
+  "key.chat", "key.command", "key.drop", "key.forward", "key.fullscreen", "key.hotbar.1",
+  "key.hotbar.2", "key.hotbar.3", "key.hotbar.4", "key.hotbar.5", "key.hotbar.6", "key.hotbar.7",
+  "key.hotbar.8", "key.hotbar.9", "key.inventory", "key.jump", "key.left",
+  "key.loadToolbarActivator", "key.pickItem", "key.playerlist", "key.right",
+  "key.saveToolbarActivator", "key.screenshot", "key.smoothCamera", "key.sneak",
+  "key.socialInteractions", "key.spectatorOutlines", "key.sprint", "key.swapOffhand",
+  "key.togglePerspective", "key.use"
+
   Args:
-    key_mapping_name: name of key binding, e.g. "key.hotbar.1"
+    key_mapping_name: name of key binding
     pressed: if `True`, press the bound key, otherwise unpress it
 
   Since: v4.0
