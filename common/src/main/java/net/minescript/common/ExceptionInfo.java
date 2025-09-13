bcfe4555aa3dbabca79db5f6a169ae97d7cb62c7
Greg Christiana <maxuser@minescript.net>
2025-09-13 02:42:12 -0400

    Improve exception handling between Java and Python
    When Java code throws an exception that's passed to a Python caller:
    1. preserve the chain of exception causes
    2. echo the descriptions of the exception causes to the chat
    3. pretty print the stack trace to logs/latest.log for legibility

--- common/src/main/java/net/minescript/common/ExceptionInfo.java
+++ common/src/main/java/net/minescript/common/ExceptionInfo.java
@@ -5,12 +5,18 @@ package net.minescript.common;
 
 import com.google.common.collect.ImmutableList;
 import java.util.List;
+import java.util.Optional;
 
-public record ExceptionInfo(String type, String message, String desc, List<StackElement> stack) {
+public record ExceptionInfo(
+    String type,
+    String message,
+    String desc,
+    List<StackElement> stack,
+    /* nullable */ ExceptionInfo cause) {
 
   public record StackElement(String file, String method, int line) {}
 
-  public static ExceptionInfo fromException(Exception e) {
+  public static ExceptionInfo fromException(Throwable e) {
     var type = e.getClass().getName();
     var desc = e.toString();
     var stackBuilder = new ImmutableList.Builder<StackElement>();
@@ -33,6 +39,7 @@ public record ExceptionInfo(String type, String message, String desc, List<Stack
       stackBuilder.add(
           new StackElement(fileName, element.getMethodName(), element.getLineNumber()));
     }
-    return new ExceptionInfo(type, e.getMessage(), desc, stackBuilder.build());
+    var cause = Optional.ofNullable(e.getCause()).map(ExceptionInfo::fromException);
+    return new ExceptionInfo(type, e.getMessage(), desc, stackBuilder.build(), cause.orElse(null));
   }
 }
