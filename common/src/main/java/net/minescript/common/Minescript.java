b0144745c86adf4dc7b22ec5a109fce472a78745
Greg Christiana <maxuser@minescript.net>
2026-09-19 01:21:51 -0400

    Make chat text gray for empty Minescript commands

--- common/src/main/java/net/minescript/common/Minescript.java
+++ common/src/main/java/net/minescript/common/Minescript.java
@@ -1817,6 +1817,9 @@ public class Minescript {
         if (completions.size() == 1 && hasFullMatch) {
           chatEditBox.setTextColor(0xff5ee85e); // green
           commandSuggestions = new ArrayList<>();
+        } else if (command.isEmpty() || command.equals("\\")) {
+          chatEditBox.setTextColor(0xffaaaaaa); // light gray
+          commandSuggestions = new ArrayList<>();
         } else {
           List<String> newCommandSuggestions = new ArrayList<>();
           newCommandSuggestions.addAll(completions);
